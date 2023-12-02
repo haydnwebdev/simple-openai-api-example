8319838020864af5962df32b7dc947af9f40688e
Haydn Ellen <haydnwebdev@gmail.com>
2023-12-02 15:32:40 +0000

    Added docstring with API and Usage Information.

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ import os
 from dotenv import load_dotenv
 from openai import OpenAI
 
+"""
+A simple command line utility to demonstrate how to make a call to
+the OpenAI API. Rename .env to .env.development and then place your
+OpenAI API key in the file.
+For more information about the API go to:
+https://platform.openai.com/docs/overview
+
+Usage: python main.py
+At the prompt, enter a query. The API will return a response.
+Uses gpt-3.5-turbo as it's cheap. Change to gpt-4-turbo for better results,
+but at a higher cost.
+"""
+
 # load .env file
 env = os.getenv('ENVIRONMENT', 'development')
 dotenv_path = f".env.{env}"
@@ -18,6 +31,7 @@ if prompt != "":
     # retrieve api key
     client.api_key = os.getenv("OPENAI_API_KEY")
 
+    # generate response
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[
@@ -28,6 +42,8 @@ if prompt != "":
         ],
         max_tokens=500,
     )
+    
+    # print response to the console
     print("-" * len(prompt))
     print(prompt.title())
     print("-" * len(prompt))
